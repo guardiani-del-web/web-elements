d057549656276467d8b131ee3c569cc2848fa034
Gabriel <g.mayta.trigozo@accenture.com>
2020-05-08 21:08:29 +0200

    closes #27

--- src/components/we-checkbox/checkbox.stories.js
+++ src/components/we-checkbox/checkbox.stories.js
@@ -7,7 +7,7 @@ export default {
 
 export const Basic = () => {
   return `
-    <we-checkbox value="Banana">Banana</we-checkbox>
+    <we-checkbox checked value="Banana">Banana</we-checkbox>
   `;
 }
 
