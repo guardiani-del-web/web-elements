4f52d11f46612344ae3b498fc1a700fe53f80422
FAngelo94 <a.falci@live.it>
2020-06-01 09:33:23 +0200

    modal basic function done

--- utils_scripts/print_scss_prop.py
+++ utils_scripts/print_scss_prop.py
@@ -1,35 +1,5 @@
 scss_file = '''
-:host {
-  display: block;
-}
-
-.back {
-  display: none;
-  width: 100vw;
-  height: 100vh;
-  background: var(--back--background, rgba(128, 128, 128, 0.35));
-  position: fixed;
-  justify-content: var(--back--justify-content, center);
-  align-items: var(--back--align-items, center);
-
-  &.visible {
-    display: flex;
-  }
-}
-
-.modal {
-  display: var(--modal--display, flex);
-  background: var(--modal--background, rgba(255, 255, 255, 1));
-  width: var(--modal--width, 12.500em);
-  height: var(--modal--height, 18.750em);  
-  padding: var(--modal--padding, 0.625em);
-  border-radius: var(--modal--border-radius, 0.625em);
-  border: var(--modal--border, none);
-  justify-content: var(--modal--justify-content, center);
-  align-items: var(--modal--align-items, unset);
-}
-
-
+Insert here scss you want transform in comments
 '''
 
 def print_scss_prop():
