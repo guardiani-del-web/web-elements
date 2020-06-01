edf1b5a57ca2361ad7a95766e1270d4465262964
fdanise <daniseferdinando@gmail.com>
2020-06-01 16:39:20 +0200

    #138 fixed prettier

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,34 +4,34 @@ import { postcss } from '@stencil/postcss';
 import autoprefixer from 'autoprefixer';
 
 export const config: Config = {
-    namespace: 'web-elements',
-    taskQueue: 'async',
-    outputTargets: [
-        {
-            type: 'dist',
-            esmLoaderPath: '../loader'
-        },
-        {
-            type: 'docs-readme',
-            footer: ''
-        },
-        {
-            type: 'docs-json',
-            file: 'custom-elements.json'
-        },
-        {
-            type: 'www',
-            serviceWorker: null // disable service workers
-        }
-    ],
-    globalStyle: 'src/scss/index.scss',
-    devServer: {
-        openBrowser: false
+  namespace: 'web-elements',
+  taskQueue: 'async',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
     },
-    plugins: [
-        sass(),
-        postcss({
-            plugins: [autoprefixer()]
-        })
-    ]
+    {
+      type: 'docs-readme',
+      footer: ''
+    },
+    {
+      type: 'docs-json',
+      file: 'custom-elements.json'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ],
+  globalStyle: 'src/scss/index.scss',
+  devServer: {
+    openBrowser: false
+  },
+  plugins: [
+    sass(),
+    postcss({
+      plugins: [autoprefixer()]
+    })
+  ]
 };
