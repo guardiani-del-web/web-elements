daa0b991eef0ae6d0b59d0dd03b0c0e8b045395e
Angelo Falci <angelo.falci@external.fcagroup.com>
2020-06-09 12:08:15 +0200

    button completed

--- utils_scripts/create_css_code.py
+++ utils_scripts/create_css_code.py
@@ -1,10 +1,10 @@
-props = ['width', 'height'] 
+props = ['width', 'height', 'padding','margin','display','border','border-radius','background','cursor','color','text-align','font-size','text-decoration']
+
+default_values = ['auto','auto','0.125em','0','block','none','0','(211, 211, 211, 1)','auto','(0, 0, 0, 1)','center','1em','none']
 component = 'button'
 
 def print_scss_prop():
-    for row in scss_file.split('\n'):
-        if(len(row.split('var'))>1):
-            prop = row.split('var')[1].split(',')[0][1::]
-            default = ",".join(row.split('var')[1].split(',')[1::])[1:-2]
-            print('* @prop '+prop+":  <br> Default: "+default)
+    for p,d in zip(props,default_values):
+        print(p+": var(--"+component+"--"+p+", "+ d+");")
+        
 print_scss_prop()
